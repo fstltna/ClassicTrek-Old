2f86545704783a8894c04228f3c2e5f3aae953c9
Marisa Giancarla <fstltna@me.com>
2015-12-06 03:56:59 -0800

    First Checkin of Project

---
+++ Classic TrekTests/Classic_TrekTests.swift
@@ -0,0 +1,36 @@
+//
+//  Classic_TrekTests.swift
+//  Classic TrekTests
+//
+//  Created by Marisa Giancarla on 12/6/15.
+//  Copyright © 2015 Pocketfiction. All rights reserved.
+//
+
+import XCTest
+@testable import Classic_Trek
+
+class Classic_TrekTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
