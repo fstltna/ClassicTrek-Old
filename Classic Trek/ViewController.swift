48eb7c5c604ef6d80fa96be3b48850731a08570e
Marisa Giancarla <fstltna@me.com>
2015-12-06 07:32:07 -0800

    Add in soundtrack MP3 file, start player code

--- Classic Trek/ViewController.swift
+++ Classic Trek/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var music:AVAudioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        prepareAudios()
+        music.play()
     }
-
+    
+    func prepareAudios() {
+        
+        var path = NSBundle.mainBundle().pathForResource("TrekSoundtrack", ofType: "mp3")
+        music = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: path!), error: nil)
+        music.prepareToPlay()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
