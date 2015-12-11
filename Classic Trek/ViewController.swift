008fce0479a650450b99d3e588119fb3d4966898
Marisa Giancarla <fstltna@me.com>
2015-12-11 15:35:25 -0800

    Changed screen for filling up space

--- Classic Trek/ViewController.swift
+++ Classic Trek/ViewController.swift
@@ -13,10 +13,11 @@ class ViewController: UIViewController {
     @IBOutlet var textView: UITextView!
     @IBAction func MoveButtonAction(sender: AnyObject) {
         
-        var userinput = inputField.text
+        let userinput = inputField.text
         print(userinput)
         // Do here whatever you want with the user's input!
     }
+    @IBOutlet var bottomHeight: NSLayoutConstraint!
     
     @IBOutlet var inputField: UITextField!
     
@@ -24,8 +25,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+
         // Do any additional setup after loading the view, typically from a nib.
-        var bgMusicURL:NSURL = NSBundle.mainBundle().URLForResource("TrekSoundtrack", withExtension: "mp3")!
+        let bgMusicURL:NSURL = NSBundle.mainBundle().URLForResource("TrekSoundtrack", withExtension: "mp3")!
         do { bgMusic = try AVAudioPlayer(contentsOfURL: bgMusicURL, fileTypeHint: nil) } catch _ { return print("file not found") }
         bgMusic.numberOfLoops = 1
         bgMusic.prepareToPlay()
@@ -36,7 +41,35 @@ class ViewController: UIViewController {
         updateTextView(inputfortextarea)
     }
     
+    func keyboardWillShow(notification:NSNotification) {
+        adjustingHeight(true, notification: notification)
+    }
     
+    func keyboardWillHide(notification:NSNotification) {
+        adjustingHeight(false, notification: notification)
+    }
+    
+    func adjustingHeight(show:Bool, notification:NSNotification) {
+        // 1
+        var userInfo = notification.userInfo!
+        // 2
+        let keyboardFrame:CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).CGRectValue()
+        // 3
+        let animationDurarion = userInfo[UIKeyboardAnimationDurationUserInfoKey] as! NSTimeInterval
+        // 4
+        let changeInHeight = (CGRectGetHeight(keyboardFrame)) * (show ? 1 : -1)
+        //5
+        UIView.animateWithDuration(animationDurarion, animations: { () -> Void in
+            self.bottomHeight.constant += changeInHeight
+        })
+        
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+
     
     // Whatever passed in the updateTextView Function will be displayed in the textView
     func updateTextView(inputText: String) -> String {
